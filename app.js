98822c7dcc5d4b8e81599797fe75bbc3c8207362
Mudit Tiwari <mudittiwari2000@gmail.com>
2020-12-20 23:40:07 +0530

    Till "Reusable Components with Props"

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-Vue.component('click-counter', {
-  template: '#click-counter-template',
-  data() {
-    return {
-      count: 0
-    };
+Vue.component('plan', {
+  template: '#plan-template',
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
   }
 });
 
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    plans: [
+      {
+        id: 1,
+        name: 'The Circus'
+      },
+      {
+        id: 2,
+        name: 'The Sintra'
+      },
+      {
+        id: 3,
+        name: 'The Corinthium'
+      }
+    ]
+  }
 });
